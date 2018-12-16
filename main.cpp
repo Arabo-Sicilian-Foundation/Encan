af517a2e6035a7290fdca136c51c43f9e048eb30
Hugo <ihfisk42@gmail.com>
2018-12-16 16:12:39 -0500

    poiuytrewq

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <time.h>
+#include <thread>
+#include "Art.h"
+#include "Vendeurs.h"
+#include "FactoryAcheteurVendeur.h"
+
+
+void genVendeurAcheteur(std::list<Acheteur*> listeAcheteur, std::list<Vendeurs<Art>*> listeVendeurArt)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		listeVendeurArt.push_back(FactoryAcheteurVendeur::createVendeurs<Art>());
+		listeAcheteur.push_back(FactoryAcheteurVendeur::createAcheteur());
+	}
+}
 
 int main()
 {
+	srand(time(NULL));
+
+	std::list<Vendeurs<Art>*> listeVendeurArt;
+	std::list<Acheteur*> listeAcheteur;
 
 	return 0;
 }
