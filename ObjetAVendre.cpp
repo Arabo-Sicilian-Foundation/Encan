4ebc900f88b2ef1e5ba674c16d76acc790b9878e
Sicilien <32875283+Sicilien@users.noreply.github.com>
2018-12-16 15:17:39 -0500

    Add files via upload

---
+++ ObjetAVendre.cpp
@@ -0,0 +1,13 @@
+#include "ObjetAVendre.h"
+
+ObjetAVendre::ObjetAVendre(std::string nomAcheteur, std::string nomVendeur, int prix)
+{
+	this->nomAcheteur = nomAcheteur;
+	this->nomVendeur = nomVendeur;
+	this->prix = prix;
+}
+
+ObjetAVendre::~ObjetAVendre()
+{
+
+}
